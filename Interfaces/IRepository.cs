8e1e0d7965e21643fb6d3e3891028f6080b4d0ab
wkuroki <45078862+wkuroki@users.noreply.github.com>
2020-02-06 11:31:15 -0300

    Add files via upload

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Locadora.Interfaces
+{
+    using Locadora.Entities;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public interface IRepository<T> where T : BaseEntity
+    {
+
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Delete(long id);
+
+        T SelectById(long id);
+
+        IList<T> SelectAll();
+
+        IQueryable<T> GetQuery();
+    }
+}
