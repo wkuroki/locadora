731311b0a0b08c27ee8c32eba8ec2777d24aeddd
wkuroki <45078862+wkuroki@users.noreply.github.com>
2020-02-06 11:24:43 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿namespace Locadora
+{
+    using FluentValidation;
+    using FluentValidation.AspNetCore;
+    using Locadora.IoC.Extensions;
+    using Locadora.Filters;
+    using Locadora.Resolvers;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+             services.AddSingleton<IConfiguration>(Configuration);
+            services.AddDependencyInjection();
+       
+            services.AddMvc(
+                options =>
+                {
+                    options.Filters.Add(typeof(ValidatorActionFilter));
+                    options.Filters.Add(typeof(CustomExceptionFilterAttribute));
+                })
+            .AddFluentValidation(fvc => fvc.RegisterValidatorsFromAssemblyContaining<Startup>())
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            ValidatorOptions.PropertyNameResolver = CamelCasePropertyNameResolver.ResolvePropertyName;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(
+                routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller}/{action=Index}/{id?}");
+                });
+         }
+    }
+}
