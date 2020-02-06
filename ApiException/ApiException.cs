62be27f0790f35680b96a8e16a54a06476934973
wkuroki <45078862+wkuroki@users.noreply.github.com>
2020-02-06 11:33:07 -0300

    Add files via upload

---
+++ ApiException/ApiException.cs
@@ -0,0 +1,30 @@
+﻿namespace Locadora.APIException
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ApiException
+    {
+        public string message { get; set; }
+        public string detail { get; set; }
+        public IList<string> errors { get; set; }
+
+        public ApiException(Exception exception)
+        {
+            message = exception.Message;
+            detail = exception.StackTrace;
+            errors = new List<string>();
+
+            CarregarInnerException(exception.InnerException);
+        }
+
+        private void CarregarInnerException(Exception exception)
+        {
+            if (exception != null)
+            {
+                errors.Add(exception.Message);
+                CarregarInnerException(exception.InnerException);
+            }
+        }
+    }
+}
