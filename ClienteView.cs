2f5fbc5095323f35556b5b630f558220ec0e243d
wkuroki <45078862+wkuroki@users.noreply.github.com>
2020-02-07 14:49:05 -0300

    Add files via upload

---
+++ ClienteView.cs
@@ -0,0 +1,32 @@
+﻿using Locadora.Entities;
+
+namespace Locadora.Models
+{
+    public class ClienteView
+    {
+
+        public ClienteView()
+        {
+        }
+
+        public ClienteView(Cliente cliente)
+        {
+            if (cliente != null)
+            {
+                IdCliente = cliente.Id;
+                Nome = cliente.Nome;
+            }
+        }
+
+        public long IdCliente { get; set; }
+        public string Nome { get; set; }
+        public Cliente ToCliente()
+        {
+            return new Cliente()
+            {
+                Id = IdCliente,
+                Nome = Nome,
+            };
+        }
+    }
+}
